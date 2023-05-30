491d44a61e939f07045d8c8f89699f08cbaf4934
noyon-jethitech <noyon.ahammad@jethitech.com>
2023-05-30 02:22:33 -0400

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-const port = config.port || 4000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+const port = config.port || 4000
 async function bootstrap() {
   try {
-    await mongoose.connect(`${config.database_url}`);
-    console.log(`Database is connected successfully`);
+    await mongoose.connect(`${config.database_url}`)
+    console.log(`Database is connected successfully`)
     app.listen(port, () => {
-      console.log(`App is running on port ${port}`);
-    });
+      console.log(`App is running on port ${port}`)
+    })
   } catch (error) {
-    console.log("Failed to connect database", error);
+    console.log('Failed to connect database', error)
   }
 }
 
-bootstrap();
+bootstrap()
